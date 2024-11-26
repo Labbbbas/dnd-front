75037d2e7e9a49c3178ab7c3670f8731facc1e09
FernandoLabastida <labastidafernando460@gmail.com>
2024-11-26 13:46:52 -0600

    feat(theme): add global theme

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import localFont from "next/font/local";
+import { theme } from "./styles/global-themes";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -22,7 +24,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ app/styles/global-themes.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      // Blue
+      main: "#005682",
+    },
+    secondary: {
+      // Red
+      main: "#dc004e",
+    },
+  },
+
+  typography: {
+    fontFamily: "Arial, sans-serif",
+  },
+});
